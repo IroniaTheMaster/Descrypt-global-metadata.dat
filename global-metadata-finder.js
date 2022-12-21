4916c5d4cbeba4d3704b9a4cde905e273dea4c06
Irönia <70151358+IroniaTheMaster@users.noreply.github.com>
2022-12-21 00:09:00 -0300

    Create global-metadata-finder.js
    Find adress offset from global-metadata.dat using frida

---
+++ global-metadata-finder.js
@@ -0,0 +1,17 @@
+Interceptor.attach(Module.findExportByName(null, 'dlopen'), {
+	onEnter: function(args) {
+		this.path = args[0].readUtf8String();
+	},
+	onLeave: function(retval) {
+		if(this.path.indexOf('libil2cpp.so') !== -1) {
+			var il2cpp = Module.findBaseAddress('libil2cpp.so');
+			console.error('[!] il2cpp : ' + il2cpp);
+			var LoadMetaDataFile = il2cpp.add(0xvalue);
+			Interceptor.attach(LoadMetaDataFile, {
+				onLeave: function(retval) {
+					console.error('[!] LoadMetaDataFile retval : ' + retval);
+				}
+			});
+		}
+	}
+});
